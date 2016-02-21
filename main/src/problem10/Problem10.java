0537905027cc3dd9035c93ca01361aee89e451d0
Vikranth Srivatsa <rama22446@gamil.com>
2016-02-21 14:53:17 -0800

    Add javadoc for problem10

--- main/src/problem10/Problem10.java
+++ main/src/problem10/Problem10.java
@@ -4,8 +4,13 @@ package problem10;
 import java.util.ArrayList;
 
 public class Problem10 {
+  /**
+   * Given: The amount the primes should be under.
+   */
   private static final double TOTAL_SUM = 2 * Math.pow(10, 6);
-
+  /**
+   * Find: The sum of all the primes under TOTAL_SUM.
+   */
   private static double SUM_OF_PRIMES;
 
   public static void main(String[] args) {
@@ -14,6 +19,11 @@ public class Problem10 {
     System.out.println(SUM_OF_PRIMES);
   }
 
+  /**
+   * Finds the Sum of all the numbers below a given number.
+   * @param givenNum The number given in the problem: two million.
+   * @return An ArrayList with all the primes below a number
+   */
   public ArrayList<Double> findAllPrimesBelowASum(double givenNum) {
     ArrayList<Double> primes = new ArrayList<>();
     primes.add(2.0);
@@ -31,6 +41,11 @@ public class Problem10 {
     return primes;
   }
 
+  /**
+   * Finds the sum of the ArrayList.
+   * @param primes A list of Primes below a certain number(two million).
+   * @return A sum of all the primes below a certain number.
+   */
   private double sumOfArrayList(ArrayList<Double> primes) {
 
     double sum = 0;
---
+++ main/src/problem11/Problem11.java
@@ -0,0 +1,7 @@
+package problem11;
+
+/**
+ * Created by vikranth on 2/21/2016.
+ */
+public class Problem11 {
+}
