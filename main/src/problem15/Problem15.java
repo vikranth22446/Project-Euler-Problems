e4713cc06eac66a229a0dd43e72c6ba9276da8cb
Vikranth Srivatsa <rama22446@gamil.com>
2016-02-25 21:08:11 -0800

    Add to problem 17

--- main/src/problem15/Problem15.java
+++ main/src/problem15/Problem15.java
@@ -4,12 +4,30 @@ package problem15;
  * Created by vikranth on 2/25/2016.
  */
 public class Problem15 {
-  int [] array = new int[];
+
+  private static int LENGTH_OF_STREET=20;
+
   public static void main(String[] args) {
-    
+    Problem15 problem15 = new Problem15();
+    System.out.println(problem15.MaximumRoutes(LENGTH_OF_STREET));
   }
-  public double MaximumRoutes(int i, int k)
-  {
-    return 0;
+
+  /**
+   * This is a simple example of permuation, since you cannot repeat positions,
+   *
+   * @param i
+   * @return
+   */
+  public double MaximumRoutes(int i) {
+    double totalNumberOfMovements = 2 * i;
+    double picking = i;
+    double combination = findFactorial(totalNumberOfMovements) /
+        (findFactorial(picking) * findFactorial(totalNumberOfMovements - picking));
+    return combination;
+  }
+
+  public double findFactorial(double n) {
+    if (n == 0) return 1;
+    return n*findFactorial(n - 1);
   }
 }
